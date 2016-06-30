9c5c7d9968b8effc7fec88be87936afb26afa2ba
Timothy George Lamballais Tessensohn <timothy.lamballais@booking.com>
2016-06-30 12:47:43 +0200

    Add support for specifying network type
    This adds support to prefix endpoint strings with a network://
    spefification. This is useful to have for several scenarios. In our
    infrastructure, we are using mounted unix sockets within containers
    to send Graphite metrics to the host os, which then forwards it to
    carbon relays.

--- g2g_test.go
+++ g2g_test.go
@@ -2,7 +2,6 @@ package g2g
 
 import (
 	"expvar"
-	"fmt"
 	"net"
 	"strings"
 	"sync"
@@ -10,18 +9,20 @@ import (
 	"time"
 )
 
-func TestPublish(t *testing.T) {
+var i = expvar.NewInt("i")
 
+func TestPublish(t *testing.T) {
+	testPub(t, "localhost:2003", NewMockGraphite(t, "tcp://:2003"))
+	testPub(t, "unix:///tmp/test.sock", NewMockGraphite(t, "unix:///tmp/test.sock"))
+}
+
+func testPub(t *testing.T, address string, mock *MockGraphite) {
 	// setup
-	port := 2003
-	mock := NewMockGraphite(t, port)
 	d := 25 * time.Millisecond
 	var g *Graphite
-	g = NewGraphite(fmt.Sprintf("localhost:%d", port), d, d)
-	time.Sleep(d)
+	g = NewGraphite(address, d, d)
 
 	// register, wait, check
-	i := expvar.NewInt("i")
 	i.Set(34)
 	g.Register("test.foo.i", i)
 
@@ -84,22 +85,22 @@ func TestRoundFloat(t *testing.T) {
 //
 
 type MockGraphite struct {
-	t     *testing.T
-	port  int
-	count int
-	mtx   sync.Mutex
-	ln    net.Listener
-	done  chan bool
+	t       *testing.T
+	address string
+	count   int
+	mtx     sync.Mutex
+	ln      net.Listener
+	done    chan bool
 }
 
-func NewMockGraphite(t *testing.T, port int) *MockGraphite {
+func NewMockGraphite(t *testing.T, address string) *MockGraphite {
 	m := &MockGraphite{
-		t:     t,
-		port:  port,
-		count: 0,
-		mtx:   sync.Mutex{},
-		ln:    nil,
-		done:  make(chan bool, 1),
+		t:       t,
+		address: address,
+		count:   0,
+		mtx:     sync.Mutex{},
+		ln:      nil,
+		done:    make(chan bool, 1),
 	}
 	go m.loop()
 	return m
@@ -117,7 +118,8 @@ func (m *MockGraphite) Shutdown() {
 }
 
 func (m *MockGraphite) loop() {
-	ln, err := net.Listen("tcp", fmt.Sprintf(":%d", m.port))
+	network, address := SplitEndpoint(m.address)
+	ln, err := net.Listen(network, address)
 	if err != nil {
 		panic(err)
 	}
